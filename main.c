6a3d71a93457ea1034854727d5d9a7681d53f1cc
Pine <2277367263@qq.com>
2023-05-31 08:55:08 +0800

    feat: 添加BPB信息输出

--- main.c
+++ main.c
@@ -5,16 +5,18 @@
 #include "fs.h"
 #include "debug.h"
 
-#define VOL_NAME 'h' /* 卷标，根据自己的分区进行调整 */
+#define VOL_NAME 'g' /* 卷标，根据自己的分区进行调整 */
 
 struct bios_pram_block bpb; /* 定义全局PBP */
 
 void parse_cmd(char *input, int *opt, char *cmd);
+void print_bpb(struct bios_pram_block *bpb);
 
 int main(int argc, char *argv[])
 {
-	disk_open_vol(VOL_NAME);	/* 打开分区 */
-	disk_read_bpb(&bpb);		/* 读取BIOS参数块 */
+	disk_open_vol(VOL_NAME); /* 打开分区 */
+	disk_read_bpb(&bpb);	 /* 读取BIOS参数块 */
+	print_bpb(&bpb);
 
 	const int SIZE = 1000;
 	int opt;
@@ -23,7 +25,6 @@ int main(int argc, char *argv[])
 
 	while (1)
 	{
-		// gets_s(cmd, SIZE);
 		gets(cmd);
 		parse_cmd(cmd, &opt, arg);
 
@@ -58,6 +59,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+/* 解析命令内容 */
 void parse_cmd(char *cmd, int *opt, char *arg)
 {
 	int i = 0;
@@ -83,4 +85,19 @@ void parse_cmd(char *cmd, int *opt, char *arg)
 	else
 		arg[0] = '\0';
 	return;
+}
+
+/* 输出bios_pram_block中的内容 */
+void print_bpb(struct bios_pram_block *bpb)
+{
+	printf("文件系统类型: %s\n", bpb->fs_type);
+	printf("卷标ID: %u\n", bpb->vol_id);
+	printf("卷标名: %s\n\n", bpb->vol_lab);
+	printf("每个扇区的字节数: %u\n", bpb->byte_per_sec);
+	printf("每个簇的扇区数: %u\n", bpb->sec_per_clus);
+	printf("保留扇区总数: %u\n", bpb->rsvd_sec_cnt);
+	printf("FAT数量: %u\n", bpb->num_fats);
+	printf("根目录条目数量上限: %u\n", bpb->root_ent_cnt);
+	printf("每个FAT占用扇区数: %u\n", bpb->sec_per_fat);
+	printf("扇区总数: %u\n", bpb->tot_sec);
 }
