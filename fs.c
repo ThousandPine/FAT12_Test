5d3d7ad47434399295332d0fdd9f161ee4f105ca
Pine <2277367263@qq.com>
2023-06-05 19:25:25 +0800

    chore: 简单调整
    添加了空的函数实现，使初始代码能够正常编译
    删除了README后面未能补充的说明
    
    感冒延误了工期，这个模板只能暂时这样草草完结了（悲

---
+++ fs.c
@@ -0,0 +1,39 @@
+/*
+ * 显示文件列表 
+ */
+void ls(const char *dir)
+{
+
+}
+
+/*
+ * 切换目录
+ */
+void cd(const char *dir)
+{
+    
+}
+
+/*
+ * 创建目录
+ */
+void mkdir(const char *dir)
+{
+
+}
+
+/*
+ * 创建文件
+ */
+void touch(const char *dir)
+{
+
+}
+
+/*
+ * 删除文件/目录
+ */
+void rm(const char *dir)
+{
+
+}
